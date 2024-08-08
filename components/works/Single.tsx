cd54060e75affec50aae8d97c0617688be6dc21b
chaotic-justice <deepocean.2900@gmail.com>
2024-08-08 14:44:23 -0400

    fix more type errors

--- components/works/Single.tsx
+++ components/works/Single.tsx
@@ -6,7 +6,7 @@ import { Separator } from "@/components/ui/separator"
 import { ArtworkQuery, Author } from "@/tina/__generated__/types"
 import { format } from "date-fns"
 import Image from "next/image"
-import { useState, MouseEvent, useRef, useEffect } from "react"
+import { useState } from "react"
 import ReactPlayer from "react-player/youtube"
 import { useTina } from "tinacms/dist/react"
 
@@ -34,11 +34,6 @@ const Single = (props: Props) => {
     formattedDate = format(date, "MMM dd, yyyy")
   }
   const { author } = artwork
-  let displayName = author?.displayNames?.find((name) => name?.lang === props.lang)
-  if (!displayName && author?.displayNames && author?.displayNames?.length > 0) {
-    displayName = author?.displayNames[0]
-  }
-
   const [showPrev, setShowPrev] = useState(false)
   const [showNext, setShowNext] = useState(false)
 
@@ -81,62 +76,49 @@ const Single = (props: Props) => {
               )}
             </Carousel>
             <div className="p-6 sm:p-8">
-              {artwork.varcharBlocks
-                ?.filter((block) => block?.__typename === "ArtworkVarcharBlocksDescriptiveBlock")
-                .map((block, i) => {
-                  const subtitle = props.lang === "en" ? block?.subtitle_en : block?.subtitle_ja
-                  const paragraph = props.lang === "en" ? block?.paragraph_en : block?.paragraph_ja
-                  return (
-                    <div key={i} className="flex flex-col [&>:not(:last-child)]:mt-6 px-8">
-                      <p className="text-xs sm:text-base font-medium mb-2">{subtitle}</p>
-                      <p className="text-xs sm:text-base sm:leading-7 tracking-wider">{paragraph}</p>
-                    </div>
-                  )
-                })}
-              <Separator className="my-4" />
-              {artwork.videoLink && (
-                <div className="flex justify-center w-full my-6 sm:mt-12">
-                  <ReactPlayer url={artwork.videoLink} width={600} height={400} light controls />
-                </div>
-              )}
-              {artwork.varcharBlocks
-                ?.filter((block) => block?.__typename === "ArtworkVarcharBlocksProcedureBlock")
-                .map((block, i) => {
-                  const paragraph = props.lang === "en" ? block?.paragraph_en : block?.paragraph_ja
-                  return (
-                    <div key={i} className="flex flex-col space-y-3 mt-8 sm:mt-12 px-8">
-                      <AspectRatio ratio={16 / 9} className="bg-muted">
-                        <Image src={block.imgSrc || "/placeholder.svg"} alt="Artwork" fill className="rounded-md object-cover" />
-                      </AspectRatio>
-                      <p className="text-xs sm:text-base sm:leading-7 tracking-wider">{paragraph}</p>
-                    </div>
-                  )
-                })}
+              {
+                artwork.varcharBlocks
+                  ?.filter((block) => block?.__typename === "ArtworkVarcharBlocksDescriptiveBlock")
+                  .map((block, i) => {
+                    // @ts-expect-error
+                    const subtitle = props.lang === "en" ? block?.subtitle_en : block?.subtitle_ja
+                    const paragraph = props.lang === "en" ? block?.paragraph_en : block?.paragraph_ja
+                    return (
+                      <div key={i} className="flex flex-col [&>:not(:last-child)]:mt-6 px-8">
+                        <p className="text-xs sm:text-base font-medium mb-2">{subtitle}</p>
+                        <p className="text-xs sm:text-base sm:leading-7 tracking-wider">{paragraph}</p>
+                      </div>
+                    )
+                  })
+              }
+              ;<Separator className="my-4" />
+              {
+                artwork.videoLink && (
+                  <div className="flex justify-center w-full my-6 sm:mt-12">
+                    <ReactPlayer url={artwork.videoLink} width={600} height={400} light controls />
+                  </div>
+                )
+              }
+              {
+                artwork.varcharBlocks
+                  ?.filter((block) => block?.__typename === "ArtworkVarcharBlocksProcedureBlock")
+                  .map((block, i) => {
+                    const paragraph = props.lang === "en" ? block?.paragraph_en : block?.paragraph_ja
+                    return (
+                      <div key={i} className="flex flex-col space-y-3 mt-8 sm:mt-12 px-8">
+                        <AspectRatio ratio={16 / 9} className="bg-muted">
+                          {/* @ts-expect-error */}
+                          <Image src={block.imgSrc || "/placeholder.svg"} alt="Artwork" fill className="rounded-md object-cover" />
+                        </AspectRatio>
+                        <p className="text-xs sm:text-base sm:leading-7 tracking-wider">{paragraph}</p>
+                      </div>
+                    )
+                  })
+              }
             </div>
           </div>
         </div>
       )}
-    </div>
-  )
-}
-
-const DisplayAuthor = ({ author, lang }: { author: Author | null; lang: string }) => {
-  let displayName = author?.displayNames?.find((name) => name?.lang === lang)
-  if (!displayName && author?.displayNames && author?.displayNames?.length > 0) {
-    displayName = author?.displayNames[0]
-  }
-  return (
-    <div className="flex items-center justify-center mt-2 mb-8 sm:mb-12">
-      {author && (
-        <>
-          <div className="flex-shrink-0 mr-4">
-            <Image className="h-14 w-14 object-cover rounded-full shadow-sm" src={author.avatar || "/placeholder.svg"} alt={displayName?.value || "avatar"} width={500} height={500} />
-          </div>
-          <p className="text-base font-medium text-gray-600 group-hover:text-gray-800">{displayName?.value}</p>
-          <span className="font-bold text-gray-200 mx-2">—</span>
-        </>
-      )}
-      {/* <p className="text-base text-gray-400 group-hover:text-gray-500">{formattedDate}</p> */}
     </div>
   )
 }
